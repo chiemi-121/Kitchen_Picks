0b19b62c07933aafe468fe6be9c210fef3a77031
chiemi-121 <c.takano2525@gmail.com>
2026-07-11 14:25:41 +0000

    1stデプロイ+テスト完了

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -14,6 +14,20 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
   test "should logout" do
     post login_url, params: { email: users(:one).email, password: "password" }
     delete logout_url
-    assert_redirected_to root_path
+    assert_redirected_to about_path
+
+    get mypage_url
+    assert_redirected_to login_path
+  end
+
+  test "logged in header has mypage link and it is navigable" do
+    post login_url, params: { email: users(:one).email, password: "password" }
+    assert_redirected_to mypage_path
+
+    follow_redirect!
+    assert_select "a[href='#{mypage_path}']", text: "マイページ"
+
+    get mypage_url
+    assert_response :success
   end
 end
