76e9f991283ab87fee00f78fe2bfeebabec5e7a7
chiemi-121 <c.takano2525@gmail.com>
2026-07-23 11:18:04 +0000

    Set Rails time zone to Tokyo

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module KitchenPicks
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = "Tokyo"
 
     config.active_support.use_solid_cache = false
     
